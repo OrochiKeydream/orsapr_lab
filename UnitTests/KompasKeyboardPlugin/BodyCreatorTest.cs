886dad0bf0f74797a78273da3055bd9ea34f5f0e
Orochi <orochi.keydream@gmail.com>
2016-05-23 15:25:35 +0600

    Удалены неиспользуемые директивы using

--- UnitTests/KompasKeyboardPlugin/BodyCreatorTest.cs
+++ UnitTests/KompasKeyboardPlugin/BodyCreatorTest.cs
@@ -1,13 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using KompasKeyboardPlugin;
 using NUnit.Framework;
-using NUnit.Framework.Constraints;
 using Kompas6API5;
-using Kompas6Constants3D;
 
 namespace UnitTests.KompasKeyboardPlugin
 {
@@ -26,7 +20,8 @@ namespace UnitTests.KompasKeyboardPlugin
                 => bodyCreatorObject.Build(document3DNull,
                 keyboardDataNull));
 
-            Assert.That(ex.Message, Is.EqualTo("Метод ссылается на null объект."));
+            Assert.That(ex.Message, Is.EqualTo("Метод ссылается на null" +
+                                               " объект."));
         }
     }
 
--- UnitTests/KompasKeyboardPlugin/KeyboardParametrsStorageTest.cs
+++ UnitTests/KompasKeyboardPlugin/KeyboardParametrsStorageTest.cs
@@ -1,11 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using KompasKeyboardPlugin;
+﻿using KompasKeyboardPlugin;
 using NUnit.Framework;
-using NUnit.Framework.Constraints;
 
 namespace UnitTests.KompasKeyboardPlugin
 {
